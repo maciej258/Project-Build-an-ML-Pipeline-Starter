5c558fba16f338d0b0ed772331af23194aa7c3b5
maciej258 <maciejgeisheimer@gmail.com>
2025-10-21 20:01:33 -0400

    edit train random forest

--- main.py
+++ main.py
@@ -118,9 +118,21 @@ def go(config: DictConfig):
             ##################
             # Implement here #
             ##################
+            _ = mlflow.run(
+                os.path.join(root_path, "src", "train_random_forest"),
+                "main",
+                parameters={
+                    "trainval_artifact": "trainval_data.csv:latest",
+                    "val_size": config['modeling']['val_size'],
+                    "random_seed": config['modeling']['random_seed'],
+                    "rf_config": rf_config,
+                    "max_tfidf_features": config['modeling']['max_tfidf_features'],
+                    "output_artifact": "random_forest_export"
+                },
+            )
             
 
-            pass
+        
 
         if "test_regression_model" in active_steps:
 
